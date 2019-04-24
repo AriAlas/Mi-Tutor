f36b6e29725b0cfaf059a847ce4318aeeaee6bf5
alexaoswald <alexaoswald1@gmail.com>
2019-04-24 13:56:00 -0400

    trying to load map after input comes back as lat,lng

--- client/src/components/Remote/index.js
+++ client/src/components/Remote/index.js
@@ -6,19 +6,13 @@ class Remote extends React.Component {
     state = {
         tutors: []
     }
-
-
     loadTutors = () => {
         API.getRemote().then(res => this.setState({ tutors: res.data }))
             .catch(err => console.log(err))
-
     }
     componentDidMount = () => {
         this.loadTutors();
-
     }
-
-
     render() {
         return (
             <div>
@@ -29,19 +23,18 @@ class Remote extends React.Component {
                             <div className="row" key={tutor.id}>
                                 <div className="col s10">
                                     <div className="card-panel white hoverable">
-                                        <div class="row valign-wrapper">
-                                            <div class="col s2">
-                                                <img src={tutor.profileImage ? tutor.profileImage : `https://via.placeholder.com/150`} alt="" class="circle responsive-img" />
+                                        <div className="row valign-wrapper">
+                                            <div className="col s2">
+                                                <img src={tutor.profileImage ? tutor.profileImage : `https://via.placeholder.com/150`} alt="" className="circle responsive-img" />
                                             </div>
-                                            <div class="col s8">
-                                                <span class="black-text">
+                                            <div className="col s8">
+                                                <span className="black-text">
                                                     <h4>{tutor.first_name}{" "}{tutor.last_name}</h4>
                                                     <p>{tutor.bio}</p>
-
                                                 </span>
                                             </div>
-                                            <div class="col s2">
-                                                <span class="black-text">
+                                            <div className="col s2">
+                                                <span className="black-text">
                                                     <h4>Subjects:</h4>
                                                     <ul className="collection">
                                                         {tutor.subjects.split(",").map(subject => (
@@ -50,19 +43,14 @@ class Remote extends React.Component {
                                                     </ul>
                                                 </span>
                                                 {/* Modal button */}
-                                                <a class="waves-effect waves-light btn modal-trigger" href="#modal4"><i class="material-icons left">email</i>Contact</a>
-
-
+                                                <a className="waves-effect waves-light btn modal-trigger" href="#modal4"><i className="material-icons left">email</i>Contact</a>
                                                   {/* Modal Structure */}
-
                                                   <div id="modal4" className="modal">
                                     <div className="modal-content">
                                         <h4>Contact me</h4>
-
                                         <form action="">
                                             <div className="row">
                                                 <div className="input-field col s12">
-
                                                     <input id="bio"
                                                         type="text"
                                                         className="validate"
@@ -72,12 +60,8 @@ class Remote extends React.Component {
                                                     ></input>
                                                     <label htmlFor="bio">Tell us a little about yourself!</label>
                                                 </div>
-                                            </div>
-                                            
-
-
+                                            </div>                               
                                         </form>
-
                                     </div>
                                     <div className="modal-footer">
                                     <button className="btn waves-effect waves-light light-blue darken-4 modal-close"
@@ -87,24 +71,15 @@ class Remote extends React.Component {
                                                 onClick={this.onClickBio}>Submit
                                                  <i className="material-icons right">send</i>
                                             </button> {" "}
-                                        <button href="#!" className="modal-close btn waves-effect  light-blue darken-4">Close</button>
+                                        <button href="#!" className="modal-close btn light-blue darken-4">Close</button>
                                     </div>
-
                                 </div>
-
-
-
-
                                             </div>
                                         </div>
-
                                     </div>
                                 </div>
                             </div>
-
                         ))}
-
-
                     </div>
                 </section>
             </div>
--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -3,48 +3,46 @@ import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
 import Map from "../components/Map";
 import SearchBar from "../components/SearchBar";
-import API from "../utils/API"
 import SearchButton from "../components/SearchButton";
 
 class SearchResults extends Component {
-    constructor(){
-      super()
-      this.state = {
-        search: "",
-        lat:"",
-        lng:""
-      }
-    }
+    // constructor(){
+    //   super()
+    //   this.state = {
+    //     search: "",
+    //     lat:"",
+    //     lng:""
+    //   }
+    // }
 
-    handleInput = (event) =>{
-      const value = event.target.value;
-      const name = event.target.name;
-      this.setState({
-        [name]:value
-      }) 
-    }
+    // handleInput = (event) =>{
+    //   const value = event.target.value;
+    //   const name = event.target.name;
+    //   this.setState({
+    //     [name]:value
+    //   }) 
+    // }
 
-    searchGeo = ()=>{
-      API.getFromGeo(this.state.search).then((res)=>{
-        console.log(res.data.results[0].geometry.location.lat)
-        console.log(res.data.results[0].geometry.location.lng)
-        this.setState({
-          search:"",
-          lat: res.data.results[0].geometry.location.lat,
-          lng: res.data.results[0].geometry.location.lng
-        })
-      }).catch(err =>{
-        return err
-      })
-
-    }
+    // searchGeo = ()=>{
+    //   API.getFromGeo(this.state.search).then((res)=>{
+    //     console.log(res.data.results[0].geometry.location.lat)
+    //     console.log(res.data.results[0].geometry.location.lng)
+    //     this.setState({
+    //       search:"",
+    //       lat: res.data.results[0].geometry.location.lat,
+    //       lng: res.data.results[0].geometry.location.lng
+    //     })
+    //   }).catch(err =>{
+    //     return err
+    //   })
+    // }
     render() {
         return (
             <div>
                 <Nav></Nav>
                 <div>
-                  <SearchBar handleInput={this.handleInput} value={this.state.search} name="search"/>
-                  <button onClick={this.searchGeo}>Click Me To Search</button>
+                  {/* <SearchBar handleInput={this.handleInput} value={this.state.search} name="search"/> */}
+                  {/* <button onClick={this.searchGeo}>Click Me To Search</button> */}
                 <Map
                 id="myMap"
                 options={{
