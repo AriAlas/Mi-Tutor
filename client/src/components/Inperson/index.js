77aab538371d44581ce689018c6860e8f4782170
alexaoswald <alexaoswald1@gmail.com>
2019-04-29 10:56:37 -0400

    styling remote and inperson collections

--- client/src/components/Inperson/index.js
+++ client/src/components/Inperson/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import { Link } from "react-router-dom"
 import API from "../../utils/API";
 import M from "materialize-css";
 
@@ -18,30 +19,47 @@ class Inperson extends Component {
         minHeight: "150px"
     }
     imgStyle = {
-        height: "100px",
-        width: "100px"
+        height: "120px",
+        width: "120px"
     }
     render() {
+        const tutorId = this.state.tutorId
+
         return (
-        <div>
-        {this.state.tutors.map(tutor => (
-        <div key={tutor.id}>
-        <ul className="collection grey lighten-5 z-depth-1">
-            <li className="collection-item avatar valign-wrapper" style={this.collectionStyle}>
-                <img src={tutor.profileImage ? tutor.profileImage : `https://via.placeholder.com/150`} alt="" className="circle" style={this.imgStyle} ></img>
-                    <div className="container">
-                        <span className="title">{tutor.first_name}{" "}{tutor.last_name}</span>
-                            <p>{tutor.bio}<br></br>
-                            {tutor.subjects}
-                            </p>
-                            <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-                    </div>
-            </li>
-        </ul>
-        </div>
-        ))}
-    </div>
-    )
+            <div>
+                {this.state.tutors.map(tutor => (
+                    <div className="row" key={tutor.id}>
+                        <div className="col s12 m12">
+                            <ul className="collection grey lighten-5 z-depth-1">
+                                <div className="row">
+                                    <li className="collection-item avatar valign-wrapper" style={this.collectionStyle}>
+                                        <div className="col s1 m2">
+                                            <div className="valign-wrapper">
+                                                <img src={tutor.profileImage ? tutor.profileImage : `https://via.placeholder.com/150`} alt="" className="circle responsive-img" style={this.imgStyle} ></img>
+                                            </div>
+                                        </div>    
+                                        <div className="col s5 m6">
+                                            <span className="title"><h5>{tutor.first_name}{" "}{tutor.last_name}</h5></span>
+                                            <p>{tutor.bio}</p>
+                                        </div>
+                                        <div className="col s2 m3">
+                                            <ul>
+                                                {tutor.subjects.split(",").map(subject => (
+                                                <li key={subject}>{subject}</li>
+                                                ))}
+                                            </ul>
+                                        </div>     
+                                        <div className="col s2 m1">
+                                            <Link to={{pathname: "viewprofile", state: {id:tutor.id}}}   className="waves-light btn modal-trigger right" name={tutor.id} onClick={this.clickHandler} >Contact Me!</Link>
+                                        </div>
+                                    </li>
+                                </div>
+                            </ul>
+                        </div>
+                        </div>
+                ))}
+            </div>    
+        )
     }
 };
 export default Inperson;
