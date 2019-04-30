38af95483c51c01bfa73a3e7fce39be6912c51cd
arieru <arieru@gmail.com>
2019-04-30 14:43:33 -0400

    login and register style

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { login } from "../UserFuntions";
 import Nav from "../Nav";
+import "./style.css"
 
 class Login extends Component {
     constructor() {
@@ -31,19 +32,20 @@ class Login extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="login-wrapper">
                 <Nav />
             <div className="section">
-                <div className="container">
-                    <div className="row">
-                        <div className="col s4"></div>
+                <div className="container ">
+                    <div className="row login">
+                        <div className="col .hide-on-small-only m3"></div>
 
                         {/* This is the login form */}
-                        <form className="col s4" noValidate onSubmit={this.onSubmit}>
+                        <form className="col s12 m6 white-text" noValidate onSubmit={this.onSubmit}>
                             <h4>Sign In</h4>
                             <div className="row">
-                                <div className="input-field col s12">
+                                <div className="input-field col s12 ">
                                     <input type="email"
+                                    className="white-text"
                                         id="email"
                                         name="email"
                                         value={this.state.email}
@@ -55,6 +57,7 @@ class Login extends Component {
                             <div className="row">
                                 <div className="input-field col s12">
                                     <input type="password"
+                                    className="white-text"
                                         id="password"
                                         name="password"
                                         value={this.state.password}
@@ -63,14 +66,14 @@ class Login extends Component {
                                 </div>
                             </div>
                             <button 
-                            className="btn waves-light" 
+                            className="btn btn-lg waves-light transparent search-button" 
                             type="submit" 
                             name="action">Sign In
-                                <i className="material-icons right">send</i>
+                                <i className="material-icons right ">send</i>
                             </button>
 
                         </form>
-                        <div className="col s4"></div>
+                        <div className="col hide-on-small-only m3"></div>
                     </div>
                 </div>
             </div>
